41e5cb5317bcc79598aceb40d84e26b3364a0f39
s.muhammed.tanwar <s.muhammed.tanwar@accenture.com>
2020-01-17 12:43:41 +0530

    connecting aws mongo instance

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/qureshi-samaaj-db",
-    { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://3.136.96.166:27017/qureshi-samaaj-db",
+    { useNewUrlParser: true,
+     useUnifiedTopology: true 
+    });
 
